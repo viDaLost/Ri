d6a2f9e0f4e69c201f980d1721d48dca9bfc4fd9
viDaLost <vitaledanilov@gmail.com>
2025-06-02 17:41:02 +0300

    Create puzzle-images.js

---
+++ puzzle-images.js
@@ -0,0 +1,11 @@
+export const puzzlePieces = [
+  'images/puzzle/1.png',
+  'images/puzzle/2.png',
+  'images/puzzle/3.png',
+  'images/puzzle/4.png',
+  'images/puzzle/5.png',
+  'images/puzzle/6.png',
+  'images/puzzle/7.png',
+  'images/puzzle/8.png',
+  'images/puzzle/9.png'
+];
